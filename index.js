03e1529eb310637d9d0bd39cd3aea2acd554de9c
cesar <cesar.caju@academico.ifpb.edu.br>
2024-09-16 13:15:47 -0300

    Adicionando uma API de fotos + pequenas mudanças na temática do site

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
 const Usuario = require('./models/Usuario');
+let user_logado = null;
 
 const app = express();
 app.engine('handlebars', handlebars.engine({
@@ -30,15 +31,13 @@ app.post('/confirmlogin', async (req, res) => {
         const { username, senha } = req.body;
         if(username && senha){
             const user_exist = any = await Usuario.findOne({
-                where: {
-                   user: username,
-                   senha: senha
-                },
+                where: {user: username, senha: senha},
              });
+
              if(user_exist){
+                user_logado = username;
                 res.redirect('/empresa')
              } else{
-                console.log('Nome de usuário ou senha incorretos');
                 setTimeout(()=>{
                     res.redirect('/login')
                 }, 100);
@@ -50,30 +49,38 @@ app.post('/confirmlogin', async (req, res) => {
         }
 
 });
+
 app.post('/confirmregister', function(req, res){
     const { username, senha, senhaconfirm } = req.body;
     if(username && senha && senhaconfirm){
         if(senha==senhaconfirm){
             Usuario.create({
                 user: username,
-                senha: senha
+                senha: senha,
+                photo: 'https://i.pinimg.com/564x/1a/12/32/1a123232bfeaaec0a23eb0f83158e76a.jpg'
             }).then(function(){
+                user_logado = username;
                 res.redirect("/empresa")
             }).catch(function(erro){
                 res.send(`Ocorreu o erro: ${erro}`)
             })
         } else{
-            res.send('As senhas devem ser iguais')
+            setTimeout(()=>{
+                res.redirect('/register')
+            }, 100);
         }
     } else{
-        res.redirect('/register')
+        setTimeout(()=>{
+            res.redirect('/register')
+        }, 100);
     }
 })
 app.get('/empresa', function(req,res){
     Usuario.findAll({order: [['id', 'DESC']]}).then(function(listaUsuarios){
-        res.render('home', {listaUsuarios: listaUsuarios})
+        res.render('home', {listaUsuarios: listaUsuarios, user_logado})
     })
 })
+
 app.get('/deletar/:id', function(req, res){
     Usuario.destroy({where: {id: req.params.id}}).then(function(){
         res.redirect('/empresa');
