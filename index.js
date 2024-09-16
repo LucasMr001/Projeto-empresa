6743519472f5447190c0b71c557da5f45b25fb41
cesar <cesar.caju@academico.ifpb.edu.br>
2024-09-16 09:37:06 -0300

    Tornando o login e registro dos usuarios funcionais

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const handlebars = require('express-handlebars');
 const Usuario = require('./models/Usuario');
 
 const app = express();
-//configs:
 app.engine('handlebars', handlebars.engine({
-    // configs do handlebars mais atualizadas
     defaultLayout: 'principal',
     runtimeOptions:{
         allowProtoMethodsByDefault: true,
@@ -15,6 +13,8 @@ app.set('view engine', 'handlebars');
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
+
+
 //Rotas:
 app.get('/', function(req,res){
     res.render('inicial');
@@ -22,18 +22,52 @@ app.get('/', function(req,res){
 app.get('/login', function(req,res){
     res.render('login');
 })
-app.get('/registro', function(req,res){
+app.get('/register', function(req,res){
     res.render('registra');
 })
-app.post('/userlog', function(req, res){
-    Usuario.create({
-        user: req.body.username,
-        senha: req.body.senha
-    }).then(function(){
-        res.redirect("/empresa")
-    }).catch(function(erro){
-        res.send(`Ocorreu o erro: ${erro}`)
-    })
+
+app.post('/confirmlogin', async (req, res) => {
+        const { username, senha } = req.body;
+        if(username && senha){
+            const user_exist = any = await Usuario.findOne({
+                where: {
+                   user: username,
+                   senha: senha
+                },
+             });
+             if(user_exist){
+                res.redirect('/empresa')
+             } else{
+                console.log('Nome de usuário ou senha incorretos');
+                setTimeout(()=>{
+                    res.redirect('/login')
+                }, 100);
+             }
+        }else{
+            setTimeout(()=>{
+                res.redirect('/login')
+            }, 100);
+        }
+
+});
+app.post('/confirmregister', function(req, res){
+    const { username, senha, senhaconfirm } = req.body;
+    if(username && senha && senhaconfirm){
+        if(senha==senhaconfirm){
+            Usuario.create({
+                user: username,
+                senha: senha
+            }).then(function(){
+                res.redirect("/empresa")
+            }).catch(function(erro){
+                res.send(`Ocorreu o erro: ${erro}`)
+            })
+        } else{
+            res.send('As senhas devem ser iguais')
+        }
+    } else{
+        res.redirect('/register')
+    }
 })
 app.get('/empresa', function(req,res){
     Usuario.findAll({order: [['id', 'DESC']]}).then(function(listaUsuarios){
@@ -48,6 +82,12 @@ app.get('/deletar/:id', function(req, res){
         console.log(`Ocorreu o erro ${erro}`)
     })
 })
+
+
+
+
+
+
 
 app.listen('8922', function(){
     console.log("Server_ON")
